0cb97b21b7b251f5e637a0edd728c67ae3f49011
Stephen-Ibe <saintkalson@gmail.com>
2023-05-11 23:50:50 +0100

    register user dto

--- src/modules/user/dto/register.dto.ts
+++ src/modules/user/dto/register.dto.ts
@@ -1,1 +1,33 @@
-export class RegisterUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 25)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 25)
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 50)
+  @Matches(/.*[0-9].*/, { message: 'Password should contain one digit' })
+  password: string;
+}
