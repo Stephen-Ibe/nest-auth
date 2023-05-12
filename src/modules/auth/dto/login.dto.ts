4943442509c7f389ce82acc466c8407f4da20adc
Stephen-Ibe <saintkalson@gmail.com>
2023-05-12 14:11:35 +0100

    login Dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,1 +1,19 @@
-export class LoginDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 50)
+  @Matches(/.*[0-9].*/, { message: 'Password should contain one digit' })
+  password: string;
+}
